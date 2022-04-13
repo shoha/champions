3aa0a1db7673b4ddb307897133533f86dea2210f
Hannah Shore <shore.hannah.e@gmail.com>
2022-04-12 22:51:32 -0400

    useHistory hook

--- components/RollHistory.tsx
+++ components/RollHistory.tsx
@@ -1,70 +1,58 @@
-import { useToasterStore, resolveValue, ToastBar } from 'react-hot-toast'
-import type { Toast } from 'react-hot-toast'
-import { useEffect, useState } from 'react'
+import { resolveValue } from "react-hot-toast";
+import { useRollHistory } from "../hooks/useRollHistory";
 
 const Message = ({ toast }) => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      margin: '4px 10px',
-      color: 'inherit',
-      flex: '1 1 auto',
-      whiteSpace: 'pre-line'
-    }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "4px 10px",
+        color: "inherit",
+        flex: "1 1 auto",
+        whiteSpace: "pre-line",
+      }}
+    >
       {resolveValue(toast.message, toast)}
     </div>
-  )
-}
+  );
+};
 
 const HistoryItem = ({ toast }) => {
   return (
-    <div style={{
-      display: "flex",
-      alignItems: "center",
-      background: "#fff",
-      color: "#363636",
-      lineHeight: "1.3",
-      willChange: "transform",
-      boxShadow: "0 3px 10px rgba(0, 0, 0, 0.1), 0 3px 3px rgba(0, 0, 0, 0.05)",
-      maxWidth: "350px",
-      pointerEvents: "auto",
-      padding: "8px 10px",
-      borderRadius: "8px",
-    }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        background: "#fff",
+        color: "#363636",
+        lineHeight: "1.3",
+        willChange: "transform",
+        boxShadow:
+          "0 3px 10px rgba(0, 0, 0, 0.1), 0 3px 3px rgba(0, 0, 0, 0.05)",
+        maxWidth: "350px",
+        pointerEvents: "auto",
+        padding: "8px 10px",
+        borderRadius: "8px",
+      }}
+    >
       <Message toast={toast}></Message>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export const RollHistory = () => {
-  const { toasts } = useToasterStore()
-  const [allToasts, setAllToasts] = useState<Toast[]>([])
-
-  useEffect(() => {
-    const mergedToasts = toasts.reduce((memo, toast) => {
-      if (!memo.find((t) => t.id === toast.id)) {
-        memo.push(toast)
-      }
-
-      return memo
-
-    }, [...allToasts])
-
-    setAllToasts([...mergedToasts])
-  }, [toasts, allToasts, setAllToasts])
+  const toasts = useRollHistory();
 
   return (
     <div className="fixed top-0 right-0 p-4 h-full bg-gray-50 w-52 border-l-2 shadow-lg">
-      < div className="mt-2 text-lg font-bold" >
-        History
-      </div >
+      <div className="mt-2 text-lg font-bold">History</div>
       <hr className="border-t-2 border-black"></hr>
       <div className="mt-2 flex gap-2 flex-col">
-        {
-          [...allToasts].reverse().map((t, i) => <HistoryItem key={i} toast={t}></HistoryItem>)
-        }
+        {[...toasts].reverse().map((t, i) => (
+          <HistoryItem key={i} toast={t}></HistoryItem>
+        ))}
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
