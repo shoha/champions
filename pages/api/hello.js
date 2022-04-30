0123093e307e97e7689cc08291b9686d3fe2971d
Hannah Shore <lookuphannahlookup@gmail.com>
2022-04-30 14:01:11 -0400

    Test exec.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { exec } from "child_process";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const date = exec("date", (error, stdout, stderr) => {
+    res.status(200).json(stdout);
+  });
 }
