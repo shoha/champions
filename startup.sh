1c097b203dcbbae7543599488bfeaf82f971f11c
Hannah Shore <lookuphannahlookup@gmail.com>
2022-04-30 15:42:10 -0400

    curl maybe

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
 if [ ! -d "java" ]; then
   mkdir java
   cd java
-  wget https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz -q
+  curl https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz -o jdk-18_linux-x64_bin.tar.gz
   tar zxf jdk-18_linux-x64_bin.tar.gz
-  # ./jdk-18.0.1/bin/java -version
   rm jdk-18_linux-x64_bin.tar.gz
 fi
